95493df7d1f24c1ffcfefd3fcb60ed5c19ae2d37
tsogplf <tsogplf@L11015.boscov.com>
2017-01-25 13:41:00 -0500

    Initial Checking in

---
+++ LocalgitRepository/TestGit/src/com/boscovs/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.boscovs;
+
+/**
+ * @author tsogplf
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Entering to test class::::");
+	}
+
+}
