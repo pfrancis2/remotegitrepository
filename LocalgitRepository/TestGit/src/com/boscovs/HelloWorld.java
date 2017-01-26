bf6f9bf3b2238ce0c5ca3020cf8bb0baa3cf1e26
tsogplf <tsogplf@L11015.boscov.com>
2017-01-26 10:51:49 -0500

    I made lot of changes.. Don't touch :::

--- LocalgitRepository/TestGit/src/com/boscovs/HelloWorld.java
+++ LocalgitRepository/TestGit/src/com/boscovs/HelloWorld.java
@@ -7,10 +7,15 @@ import java.io.Serializable;
 
 /**
  * @author tsogplf
- *
+ * This is the Helloworld, Don't touch this class.. Hard to recreate this ::: 
  */
 public class HelloWorld implements Serializable
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void check()
 	{
 		System.out.println("Just checking in.....");		
--- LocalgitRepository/TestGit/src/com/boscovs/Test.java
+++ LocalgitRepository/TestGit/src/com/boscovs/Test.java
@@ -7,12 +7,15 @@ package com.boscovs;
  * @author tsogplf
  *
  */
-public class Test {
+public class Test 
+{
 
 	/**
 	 * @param args
+	 * This is just a Test class
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Entering to test class::::");
 		HelloWorld helloWorld = new HelloWorld();
